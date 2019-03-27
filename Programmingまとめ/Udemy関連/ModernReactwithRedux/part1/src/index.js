c0db49e92ca21e5432faf4b5aaad5429e4b2ee56
kaleido <kaleido@gmail.com>
2019-03-27 20:15:35 +0900

    how to jsx

--- Programmingまとめ/Udemy関連/ModernReactwithRedux/part1/src/index.js
+++ Programmingまとめ/Udemy関連/ModernReactwithRedux/part1/src/index.js
@@ -1,12 +1,29 @@
+//import the react and ReactDom libralies
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create a react component
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App=()=>{
+  const hi="hi"
+  const func=(number)=>{
+
+    return number*number
+
+  }
+
+  return (
+  <div>
+     <h1>{hi}</h1> 
+     <p>{func(5)}</p>  
+  </div>
+  )
+}
+
+//Take the react component and show it on the screen
+
+ReactDOM.render(
+  <App />,
+  document.querySelector("#root")
+)
