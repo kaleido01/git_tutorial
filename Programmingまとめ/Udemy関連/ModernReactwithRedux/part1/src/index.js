98a0d95e952cb3d431255cdfdf5cf1c89a8f3f06
kaleido <kaleido@gmail.com>
2019-03-27 21:23:13 +0900

    faker ui

--- Programmingまとめ/Udemy関連/ModernReactwithRedux/part1/src/index.js
+++ Programmingまとめ/Udemy関連/ModernReactwithRedux/part1/src/index.js
@@ -2,21 +2,29 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import faker from 'faker';
 // create a react component
 
 const App=()=>{
-  const hi="hi"
-  const func=(number)=>{
-
-    return number*number
-
-  }
 
   return (
-  <div>
-     <h1>{hi}</h1> 
-     <p>{func(5)}</p>  
+  <div> 
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img src={faker.image.avatar()} alt="avatar"/>
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="date">Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
   </div>
   )
 }
